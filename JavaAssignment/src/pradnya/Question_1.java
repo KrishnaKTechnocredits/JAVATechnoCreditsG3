6fe763a9499d9194a0ebade42f6dbe1088c9797a
unknown <pradnyamore1115@gmail.com>
2019-01-23 01:14:42 +0530

    Question_1 assigmment main method

---
+++ JavaAssignment/src/pradnya/Question_1.java
@@ -0,0 +1,34 @@
+package pradnya;
+
+public class Question_1 {
+	
+	public int sum(int a, int b)
+	{
+		int s= a+b;
+		return s;
+	}
+	public int sub(int a, int b)
+	{
+		int s = a-b;
+		return s;       
+	}
+	public int mul(int a, int b)
+	{
+		int s = a*b;
+		return s;
+	}
+	public int div(int a, int b)
+	{
+		int s = a/b;
+		return s;
+	}
+	
+	public int rem(int a, int b)
+	{
+		int s = a%b;
+		return s;
+	}
+
+
+
+}
