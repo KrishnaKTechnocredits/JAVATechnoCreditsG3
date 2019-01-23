2025f49e28b55666e8039dc251d7f5f43f69c7cd
Your Name <ankita_stechnocredits@rediffmail.com>
2019-01-23 11:11:57 +0530

    ankita sinha added assignment1-q7

---
+++ JavaAssignment/src/ankitaS/Assignment1_Q7.java
@@ -0,0 +1,23 @@
+package ankitaS;
+
+public class Assignment1_Q7 {
+	void TimeConversion(int Seconds) { // method to convert seconds into
+										// hour:minute:second
+
+		int r = Seconds % 3600;
+		int hours = Seconds / 3600;
+		int minutes = r / 60;
+		int seconds = r / 60;
+
+		System.out.println("Time is : " + hours + ":" + minutes + ":" + seconds);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment1_Q7 a = new Assignment1_Q7();
+		int Seconds = Integer.parseInt(args[0]);
+		a.TimeConversion(Seconds);
+	}
+
+}
