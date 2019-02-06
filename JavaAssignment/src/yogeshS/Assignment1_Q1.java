3592507ee1b3ad617488b55ce66eb18ea1d7572c
YogeshTechnocredits <sawanyo@gmail.com>
2019-02-06 21:22:40 +0530

    Java Assignment Q1

--- JavaAssignment/src/yogeshS/Assignment1_Q1.java
+++ JavaAssignment/src/yogeshS/Assignment1_Q1.java
@@ -1,10 +1,29 @@
 package yogeshS;
 
 public class Assignment1_Q1 {
-	public static void main(String []args)
-	{
-		System.out.println("My First Assignmentg");
-		
-	}
+	int sum (int a, int b)
+		{
+			int c=a+b;
+			return c;
+    
+		}
+	int multiplication (int a, int b)
+		{
+			int c=a*b;
+			return c;
+    
+		}
+	int division (int a, int b)
+		{
+			int c=a%b;
+			return c;
+  
+		}
+	int minus (int a, int b)
+		{
+			int c=a-b;
+			return c;
+    
+		}	
 
 }
